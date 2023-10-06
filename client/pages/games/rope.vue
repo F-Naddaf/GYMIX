3f615f84e210f41c37c8bee407f43dc2d0039f3f
Fadi <fady-naddaf@hotmail.com>
2023-10-06 14:54:56 +0200

    Creating first game, Weight liftting

---
+++ client/pages/games/rope.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="rope-jumping">
+    <div></div>
+  </div>
+</template>
+
+<script setup>
+definePageMeta({
+  layout: "custom",
+});
+</script>
+
+<style scoped>
+.rope-jumping {
+  display: flex;
+}
+</style>
