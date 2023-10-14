73ed7828dc62d19dc3af2aded70a66b385ff2d31
Fadi <fady-naddaf@hotmail.com>
2023-10-15 01:17:44 +0200

    Completed Porgrams page'

---
+++ client/data/porgrams.js
@@ -0,0 +1,155 @@
+export default [
+  {
+    monday: [
+      {
+        trainer: "Bella Wellness",
+        Class: "Fitness",
+        Timing: "09:00 am - 10:00 am",
+      },
+      {
+        trainer: "Arnold Steel",
+        Class: "Body Building",
+        Timing: "10:00 am - 11:00 am",
+      },
+      {
+        trainer: "Emily Pace",
+        Class: "Running",
+        Timing: "02:00 pm - 03:00 pm",
+      },
+      {
+        trainer: "Lance Spinworthy",
+        Class: "Cycling",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+    ],
+  },
+  {
+    tuesday: [
+      {
+        trainer: "Rocky Jabson",
+        Class: "Boxing",
+        Timing: "09:00 am - 10:00 am",
+      },
+      {
+        trainer: "John Lewis",
+        Class: "Yoga",
+        Timing: "10:00 am - 11:00 am",
+      },
+      {
+        trainer: "Jonathan Doe",
+        Class: "Crossfit",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+    ],
+  },
+  {
+    wednesday: [
+      {
+        trainer: "Lance Spinworthy",
+        Class: "Cycling",
+        Timing: "09:00 am - 10:00 am",
+      },
+      {
+        trainer: "Arnold Steel",
+        Class: "Body Building",
+        Timing: "10:00 am - 11:00 am",
+      },
+      {
+        trainer: "Bella Wellness",
+        Class: "Fitness",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+      {
+        trainer: "Emily Pace",
+        Class: "Running",
+        Timing: "06:00 pm - 07:00 pm",
+      },
+    ],
+  },
+  {
+    thursday: [
+      {
+        trainer: "Bella Wellness",
+        Class: "Fitness",
+        Timing: "09:00 am - 10:00 am",
+      },
+      {
+        trainer: "Arnold Steel",
+        Class: "Body Building",
+        Timing: "10:00 am - 11:00 am",
+      },
+      {
+        trainer: "David Smith",
+        Class: "Power Lifting",
+        Timing: "02:00 pm - 03:00 pm",
+      },
+      {
+        trainer: "Emily Pace",
+        Class: "Running",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+    ],
+  },
+  {
+    friday: [
+      {
+        trainer: "Jonathan Doe",
+        Class: "Crossfit",
+        Timing: "09:00 am - 10:00 am",
+      },
+      {
+        trainer: "Sensei Hiro",
+        Class: "Karate",
+        Timing: "10:00 am - 11:00 am",
+      },
+      {
+        trainer: "Lily Serenity",
+        Class: "Meditation",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+      {
+        trainer: "Jack Fitster",
+        Class: "Workout",
+        Timing: "06:00 pm - 07:00 pm",
+      },
+    ],
+  },
+  {
+    saturday: [
+      {
+        trainer: "Rocky Jabson",
+        Class: "Boxing",
+        Timing: "12:00 am - 01:00 am",
+      },
+      {
+        trainer: "David Smith",
+        Class: "Power Lifting",
+        Timing: "02:00 pm - 03:00 pm",
+      },
+      {
+        trainer: "Emily Pace",
+        Class: "Running",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+    ],
+  },
+  {
+    sunday: [
+      {
+        trainer: "Bella Wellness",
+        Class: "Fitness",
+        Timing: "01:00 am - 02:00 am",
+      },
+      {
+        trainer: "John Lewis",
+        Class: "Yoga",
+        Timing: "04:00 pm - 05:00 pm",
+      },
+      {
+        trainer: "Lance Spinworthy",
+        Class: "Cycling",
+        Timing: "06:00 pm - 07:00 pm",
+      },
+    ],
+  },
+];
--- client/pages/programs.vue
+++ client/pages/programs.vue
@@ -2,15 +2,40 @@
   <div>
     <PagesHero :title="title" />
     <div class="weekdays-container">
-      <button v-for="(day, index) in weekDays" :key="index">{{ day }}</button>
+      <button
+        v-for="(day, index) in weekDays"
+        :key="index"
+        @click="filterClasses(day)"
+        :class="{ active: day === selectedDay }"
+      >
+        {{ day }}
+      </button>
+    </div>
+    <div v-if="selectedDay" class="classes">
+      <ul v-for="(classData, index) in filteredClasses" :key="index">
+        <li>
+          Class Name: <span>{{ classData.Class }}</span>
+        </li>
+        <li>
+          Timing:<span> {{ classData.Timing }}</span>
+        </li>
+        <li>
+          Trainer: <span>{{ classData.trainer }} </span>
+        </li>
+        <li>
+          <button class="join">Join Now</button>
+        </li>
+      </ul>
     </div>
   </div>
 </template>
 
 <script setup>
-import { ref } from "vue";
+import Programs from "../data/porgrams.js";
+import { ref, onMounted, computed } from "vue";
 
 const title = ref("Programs");
+const programsArray = Programs;
 
 const weekDays = [
   "Monday",
@@ -21,6 +46,21 @@ const weekDays = [
   "Saturday",
   "Sunday",
 ];
+
+const selectedDay = ref("Monday");
+
+const filterClasses = (day) => {
+  selectedDay.value = day;
+};
+
+const filteredClasses = computed(() => {
+  const selectedClasses = programsArray.find(
+    (item) => item[selectedDay.value.toLowerCase()]
+  );
+  return selectedClasses
+    ? selectedClasses[selectedDay.value.toLowerCase()]
+    : [];
+});
 </script>
 
 <style scoped>
@@ -47,13 +87,48 @@ button {
   cursor: pointer;
 }
 button:hover {
-  background: var(--primary-color);
-  border: 1px solid var(--primary-color);
+  background: var(--secondary-color);
+  border: 1px solid var(--secondary-color);
   color: white;
 }
 .active {
-  background: var(--primary-color);
-  border: 1px solid var(--primary-color);
+  background: var(--secondary-color);
+  border: 1px solid var(--secondary-color);
   color: white;
+}
+.classes {
+  margin: 20px 0 80px 0;
+}
+ul {
+  display: flex;
+  justify-content: space-between;
+  width: 80%;
+  height: 80px;
+  margin: 20px auto;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-left: 1px solid rgba(255, 255, 255, 0.5);
+  border-color: rgba(255, 255, 255, 0.3);
+  background-color: rgba(255, 255, 255, 0.1);
+  box-shadow: rgba(0, 0, 0, 0.7) 0px 3px 8px;
+  align-items: center;
+}
+li {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100%;
+  width: 100%;
+  color: rgba(255, 255, 255, 0.5);
+}
+span {
+  font-size: 18px;
+  font-weight: 800;
+  letter-spacing: 1px;
+  padding-top: 10px;
+  color: white;
+}
+.join {
+  background: #747474;
 }
 </style>
