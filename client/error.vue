d7bd631b1066cf11b0ff01995d93244ba30efc3e
Fadi <fady-naddaf@hotmail.com>
2023-10-17 11:03:01 +0200

    Fix the style for error page

--- client/error.vue
+++ client/error.vue
@@ -19,6 +19,19 @@ defineProps(["error"]);
   display: flex;
   align-items: center;
   justify-content: center;
+  position: relative;
+}
+.error-container::before {
+  content: "";
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-image: url(/_nuxt/images/background.png);
+  background-size: cover;
+  background-repeat: no-repeat;
+  opacity: 0.3;
 }
 .error-wrapper {
   display: flex;
@@ -31,15 +44,23 @@ defineProps(["error"]);
   padding: 40px;
   border-radius: 10px;
   margin: auto;
+  z-index: 20;
+  border-top: 1px solid rgba(255, 255, 255, 0.5);
+  border-left: 1px solid rgba(255, 255, 255, 0.5);
+  border-color: rgba(255, 255, 255, 0.3);
+  background-color: rgba(255, 255, 255, 0.1);
+  box-shadow: rgba(0, 0, 0, 0.7) 0px 3px 8px;
 }
 .error-wrapper h2 {
   font-size: 88px;
   font-weight: 900;
   color: var(--primary-color);
+  text-shadow: 1px 1px #ffffff;
 }
 .error-wrapper p {
   font-size: 18px;
   font-weight: 700;
+  color: white;
 }
 .error-wrapper a {
   display: flex;
