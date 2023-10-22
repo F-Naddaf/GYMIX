b8aa1312d816d734843195fd029ebac4da07170a
Fadi <fady-naddaf@hotmail.com>
2023-10-22 18:21:28 +0200

    Changing the nuxt.config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,20 +1,21 @@
 export default {
   modules: ["@nuxtjs/google-fonts"],
 
-  googleFonts: {
-    families: {
-      Roboto: true,
-      "Josefin+Sans": true,
-      Lato: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      Raleway: {
-        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      },
-    },
-  },
-  css: ["@fortawesome/fontawesome-free/css/all.css", "/assets/css/style.css"],
-  build: {
-    rollupOptions: {
-      external: ["@fortawesome/fontawesome-free/css/all.css"],
-    },
-  },
+  // googleFonts: {
+  //   families: {
+  //     Roboto: true,
+  //     "Josefin+Sans": true,
+  //     Lato: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  //     Raleway: {
+  //       wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+  //     },
+  //   },
+  // },
+  css: ["/assets/css/style.css"],
+  // css: ["@fortawesome/fontawesome-free/css/all.css", "/assets/css/style.css"],
+  // build: {
+  //   rollupOptions: {
+  //     external: ["@fortawesome/fontawesome-free/css/all.css"],
+  //   },
+  // },
 };
