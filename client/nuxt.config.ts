e6d7178d9774bae9da2fb8ebee6805266893330a
Fadi <fady-naddaf@hotmail.com>
2023-10-22 16:29:14 +0200

    Removing the fonts from nuxt.config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,13 +1,6 @@
 export default {
   modules: ["@nuxtjs/google-fonts"],
-
-  googleFonts: {
-    families: {
-      Lato: {
-        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-      },
-    },
-  },
+  
   build: {
     rollupOptions: {
       external: ["@fortawesome/fontawesome-free/css/all.css"],
