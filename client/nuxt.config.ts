322d6126f50e0f8909ca1fd9999359e244beb7a9
Fadi <fady-naddaf@hotmail.com>
2023-10-22 16:39:42 +0200

    Fixing the nuxt.config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,6 +1,17 @@
 export default {
   modules: ["@nuxtjs/google-fonts"],
-  
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+      "Josefin+Sans": true,
+      Lato: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      Raleway: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    },
+  },
+  css: ["@fortawesome/fontawesome-free/css/all.css", "/assets/css/style.css"],
   build: {
     rollupOptions: {
       external: ["@fortawesome/fontawesome-free/css/all.css"],
