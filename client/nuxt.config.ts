3abe989999c2dacc7f5fe44d735efeffa8715650
Fadi <fady-naddaf@hotmail.com>
2023-10-22 21:12:00 +0200

    Fixing the nuxt.config

--- client/nuxt.config.ts
+++ client/nuxt.config.ts
@@ -1,21 +1,33 @@
 export default {
   modules: ["@nuxtjs/google-fonts"],
 
-  // googleFonts: {
-  //   families: {
-  //     Roboto: true,
-  //     "Josefin+Sans": true,
-  //     Lato: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  //     Raleway: {
-  //       wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
-  //     },
-  //   },
-  // },
-  // css: ["/assets/css/style.css"],
-  // css: ["@fortawesome/fontawesome-free/css/all.css", "/assets/css/style.css"],
-  // build: {
-  //   rollupOptions: {
-  //     external: ["@fortawesome/fontawesome-free/css/all.css"],
-  //   },
-  // },
+  app: {
+    head: {
+      title: "GYMIX",
+      meta: [{ name: "description", content: "Sport App with Nuxt 3" }],
+      link: [
+        {
+          rel: "stylesheet",
+          href: "https://fonts.googleapis.com/icon?family=Material+Icons",
+        },
+      ],
+    },
+  },
+
+  googleFonts: {
+    families: {
+      Roboto: true,
+      "Josefin+Sans": true,
+      Lato: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      Raleway: {
+        wght: [100, 200, 300, 400, 500, 600, 700, 800, 900],
+      },
+    },
+  },
+  css: ["@fortawesome/fontawesome-free/css/all.css", "/assets/css/style.css"],
+  build: {
+    rollupOptions: {
+      external: ["@fortawesome/fontawesome-free/css/all.css"],
+    },
+  },
 };
